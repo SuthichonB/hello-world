16f31b29599ec411fa3102b3f5fb24cf21cc2b99
SuthichonB <SuthichonB@users.noreply.github.com>
2020-08-19 17:16:02 +0700

    edit

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,65 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{t1+t2+t3}}</h1>
     <div class="row">
-      <div class="col-3"> 
-      <img alt="Vue logo" src="./assets/logo.png">
-      <CsBotton/>
+      <div class="col-4"> 
+      <img src="https://i.pinimg.com/originals/78/ac/aa/78acaad3c2890c0d47f94ec7b3cce9fb.jpg" height="350" alt="">
+      <CsBotton @cc="sum1"/>
       </div>  
-        <div class="col-3"> 
-      <img alt="Vue logo" src="./assets/logo.png">
-      <CsBotton/>
+      <div class="col-4"> 
+      <img src="https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/6/63/97d1d9f934a350cee765c5ac1a466605.jpg/revision/latest?cb=20190527184444" height="350" alt="">
+      <CsBotton @cc="sum2"/>
       </div> 
-        <div class="col-3"> 
-      <img alt="Vue logo" src="./assets/logo.png">
-      <CsBotton/>
+      <div class="col-4 " > 
+      <img src="https://www.kindpng.com/picc/m/98-989089_jotaro-kujo-part-3-hd-png-download.png" height="350" alt="">
+      <CsBotton @cc="sum3"/>
       </div>   
-  </div>
+  </div><br>
+  <!----ชื่อ สกึล รหัส GPA--->
+    ชื่อ-นามสกุล
+    <input type="text" v-model="name" />
+    รหัสนักศึกษา
+    <input type="text" v-model="code" />
+    GPA
+    <input type="number" v-model="gpa" step="0.01" />
+    <hr />
+
+    แสดงชื่อ + นามสกุล + รหัสนักศึกษา + GPA
+    {{name}} {{code}} {{gpa}}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import CsBotton from './components/CsBotton.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
     CsBotton,
     
-  }
+  },
+  data: function() {
+      return{
+        total : 0,
+        t1 : 0,
+        t2 : 0,
+        t3 : 0,
+        name: "",
+        code: "",
+        gpa: 0.1, 
+      }
+    },
+  methods: {
+    sum1(counterValue){
+      this.t1 = counterValue
+    },
+    sum2(counterValue){
+      this.t2 = counterValue
+    },
+    sum3(counterValue){
+      this.t3 = counterValue
+    },
+  },
 }
 </script>
 
@@ -41,5 +71,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.col-4{
+  border: 1px solid red;
 }
 </style>
--- src/components/CsBotton.vue
+++ src/components/CsBotton.vue
@@ -1,18 +1,18 @@
 <template>
-   <div>
-   <h1> {{counter}}</h1>
+  <div>
+    <h1>{{counter}}</h1>
   <div class="row">
     <div class="col-3">
-     <button v-on:click="counter += 1"> <i class="far fa-thumbs-up"></i></button>
+     <button v-on:click="upFunction"> <i class="far fa-thumbs-up"></i></button>
    </div>
      <div class="col-3">
-     <button v-on:click="counter -= 10"><i class="far fa-thumbs-down"></i></button>
+     <button v-on:click="downFunction"><i class="far fa-thumbs-down"></i></button>
    </div>
      <div class="col-3">
-     <button v-on:click="counter += 10"><i class="far fa-heart"></i></button>
+     <button v-on:click="LFunction"><i class="far fa-heart"></i></button>
    </div>
      <div class="col-3">
-     <button v-on:click="counter = 0"><i class="fas fa-retweet"></i></button>
+     <button v-on:click="reFunction"><i class="fas fa-retweet"></i></button>
   </div>
   </div>
   </div>
@@ -25,9 +25,35 @@ export default {
         lebel: String,
     },
     data: function() {
-  return{
-    counter: 0,
-    }
+      return{
+        counter: 0,
+        fsize: 50,
+        
+      }
+    },
+
+    methods: {
+   upFunction : function(){
+      this.counter += 1
+      this.fsize += 10
+      this.$emit('cc', this.counter)
+   },
+   downFunction : function(){
+      this.counter -= 1
+      this.fsize -= 10
+      this.$emit('cc', this.counter)
+   },
+   LFunction : function(){
+      this.counter += 10
+      this.fsize += 10
+      this.$emit('cc', this.counter)
+   },
+   reFunction : function(){
+      this.counter = 0
+      this.fsize = 50
+      this.$emit('cc', this.counter)
+   },
+  
   }
 }
 </script>
